02bd04287cc69fe85575c1dc5f03edc98b1e564f
b-i-hide <b.i1993patriots@gmail.com>
2017-10-11 11:51:14 +0900

    [add] 累乗計算

--- power.go
+++ power.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(pow(3,2))
+	fmt.Println(pow(2, 16))
+	fmt.Println(pow(6,19))
+	fmt.Println(pow(4.16, 24))
+}
+
+func pow(x float64, n int) float64 {
+	v := 1.0
+	for ;n > 0; n-- {
+		v *= x
+	}
+	return v
+}
